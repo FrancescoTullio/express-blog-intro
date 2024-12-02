ea4d4f7d9bfc26a223335b698177c1d7bf5eaa3d
FrancescoTullio <francescotullio1992@gmail.com>
2024-12-02 16:20:12 +0100

    feat: chiamata

--- app.js
+++ app.js
@@ -1,14 +1,21 @@
 console.log("ciao")
-const express = require("express");
+import express from "express"
 const app = express();
 const port = 3000;
 
-// import postBlog from "./models/post.js"
+import postBlog from "./models/post.js"
 
 
-// console.log(postBlog);
+//  console.log(postBlog);
 app.get(`/`, (req, res) => {
-    console.log("hello world!")
+    res.send("hello world!")
+})
+
+app.get(`/bacheca`, (req, res) => {
+
+
+
+    res.json(postBlog)
 })
 
 
--- models/post.js
+++ models/post.js
@@ -1,10 +1,34 @@
-// const postBlog = [
-//     {
-//         titolo: "Ciambellone",
-//         contenuto: "ciao sono il contenuto, non so cosa devo essere",
-//         tag: ["#facile", "#veloce", "#economica"],
-//         img: ""
-//     }];
+ const postBlog = [
+     {
+         titolo: "Ciambellone",
+         contenuto: "ciao sono il contenuto, non so cosa devo essere",
+         tag: ["#facile", "#veloce", "#economica"],
+         img: ""
+     },
+     {
+        titolo: "Craker Barbabietola",
+        contenuto: "ciao sono il contenuto, non so cosa devo essere",
+        tag: ["#facile", "#veloce", "#economica", "#croccante", "#colorata"],
+        img: ""
+    },
+    {
+        titolo: "Pane Fritto Dolce",
+        contenuto: "ciao sono il contenuto, non so cosa devo essere",
+        tag: ["#facile", "#veloce", "#economica", "#confortfood", "#tradizionale"],
+        img: ""
+    },
+    {
+        titolo: "Pasta alla Barbabietola",
+        contenuto: "ciao sono il contenuto, non so cosa devo essere",
+        tag: ["#facile", "#veloce", "#economica", "#sana", "#colorata"],
+        img: ""
+    },
+    {
+        titolo: "Torta Paesana",
+        contenuto: "ciao sono il contenuto, non so cosa devo essere",
+        tag: ["#facile", "#veloce", "#economica", "#locale", "#tradizionale"],
+        img: ""
+    }];
 
 
-//     export default postBlog;
+     export default postBlog;
